f0e6aa0d500da408b9e8f394bb7d488258b38642
Daniel Cassidy <mail@danielcassidy.me.uk>
2020-05-06 15:09:42 +0100

    feat: create Prettier configuration

---
+++ index.ts
@@ -0,0 +1,42 @@
+import {Options} from "prettier";
+
+namespace config {
+    export interface Config extends Options {
+        overrides?: Overrides[];
+    }
+
+    export interface Overrides {
+        files: string | string[];
+        options: Options;
+    }
+}
+
+const config: config.Config = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: false,
+    quoteProps: "consistent",
+    jsxSingleQuote: false,
+    trailingComma: "none",
+    bracketSpacing: false,
+    jsxBracketSameLine: false,
+    arrowParens: "avoid",
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: "always",
+    htmlWhitespaceSensitivity: "css",
+    vueIndentScriptAndStyle: true,
+    endOfLine: "auto",
+    overrides: [
+        {
+            files: ["*.css", "*.json", "*.less", "*.sass", "*.yml"],
+            options: {
+                tabWidth: 2
+            }
+        }
+    ]
+};
+
+export = config;
